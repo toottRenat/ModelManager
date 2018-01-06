27e4a89bb8751c1cfb9da02570f64bcb62d9cfc2
toottRenat <basonchik@yandex.ru>
2018-01-06 17:39:32 +0300

    added metrics as dictionary in ModelsManager, refactored some stuff

--- models.py
+++ models.py
@@ -1,6 +1,3 @@
-"""
-ТИПА ДЕЛАЕМ АПИ
-"""
 
 import os
 import pandas as pd
@@ -39,7 +36,7 @@ def check_name_correctness(func):
 
 
 """
-training_mode:
+processing_mode:
 0 - данные для обучения читаются из файлов в заданной папке (нерекурсивно), type(training_data) is str
 1 - training_data is iterable
 2 - 
@@ -54,67 +51,62 @@ class DataProcessor:
                       '_DstHostSRVCount_', '_DstHostSameSRCPortRate_', '_SameSRVRate_',
                       '_Label_']  # последней всегда должны идти лейблы (или можно сделать их отдельную передачу)
 
-    def __init__(self, name, attack_ratio=0.05, attack_probability=0.9,
-                 attack_label=-1, training_mode=0, using_features=None, training_data=None,
-                 csv_separator='\t', num_classes=2):
+    def __init__(self, name, attack_ratio=0.05, attack_label=-1, processing_mode=0,
+                 using_features=None, training_data=None, csv_separator='\t', num_classes=2):
 
         self.name = name
         self.attack_ratio = attack_ratio
-        self.attack_probability = attack_probability
         self.attack_label = attack_label
 
         if using_features is not None:
             self.using_features = using_features
 
         if training_data is None:
-            self.training_data = CSV_WALKING_PATH
-            self.training_mode = 0
+            self.data = CSV_WALKING_PATH
+            self.processing_mode = 0
         else:
-            self.training_mode = training_mode
-            self.training_data = training_data
+            self.processing_mode = processing_mode
+            self.data = training_data
         self.csv_separator = csv_separator
         self.num_classes = num_classes
 
         self.__check_parameters_correctness()
 
-        if not self.training_mode:
-            self.training_data = self.get_data_from_path()
-
-        if self.training_data is None:
-            pass  # todo handle somehow errors
+        if not self.processing_mode:
+            self.data = self.get_data_from_path()
 
         self.fetch_classes()
         self.make_all_features_numeric()
 
     def fetch_classes(self):
         if self.num_classes == 2:
-            self.training_data[self.using_features[-1]] = self.training_data[self.using_features[-1]].apply(
+            self.data[self.using_features[-1]] = self.data[self.using_features[-1]].apply(
                 lambda x: self.attack_label if x*self.attack_label > 0 else x
             )
 
     def get_labels(self):
-        return np.array(self.training_data[self.using_features[-1]])
+        return np.array(self.data[self.using_features[-1]])
 
     def get_features(self):
         features_without_labels = deepcopy(self.using_features)
         features_without_labels.remove(self.using_features[-1])
-        return np.array(self.training_data[features_without_labels])
+        return np.array(self.data[features_without_labels])
 
     def make_all_features_numeric(self):
-        for index in self.training_data.columns.values:
-            for item in self.training_data[index]:
+        for index in self.data.columns.values:
+            for item in self.data[index]:
                 if item not in [0, '0', '0.0', []]:
                     try:
                         float(item)
                     except (ValueError, TypeError):
-                        self.training_data[index] = fit_label_encoder(self.training_data[index])
+                        self.data[index] = fit_label_encoder(self.data[index])
                     break
 
     def get_data_from_path(self):
         all_csv_files = []
         df = pd.DataFrame()
 
-        for item in os.walk(self.training_data):
+        for item in os.walk(self.data):
             for f in item[-1]:
                 if f[-1:-5:-1][::-1] == '.csv':
                     all_csv_files.append('/'.join([item[0], f]))
@@ -128,12 +120,8 @@ class DataProcessor:
             else:
                 df = df.append(df)
 
-        try:
-            indices = df[:-1]
-        except KeyError:  # some of features is not in data or df is empty
-            return None
-        else:
-            return indices[self.using_features]
+        indices = df[:-1]
+        return indices[self.using_features]
 
     def get_train_test_split(self, test_size=0.33):
         return train_test_split(self.get_features(),
@@ -144,16 +132,13 @@ class DataProcessor:
         if type(self.attack_ratio) not in (int, float):
             raise ValueError('Parameter attack_ratio should has type int or float')
 
-        if type(self.attack_probability) not in (int, float):
-            raise ValueError('Parameter attack_probability should has type int or float')
-
         try:
             list(self.using_features)
         except TypeError:
             raise ValueError('Parameter attack_probability should be iterable')
 
         try:
-            list(self.training_data)
+            list(self.data)
         except TypeError:
             raise ValueError('Parameter training_data should be iterable')
 
@@ -164,24 +149,21 @@ class DataProcessor:
         if not 0 < self.attack_ratio < 1:
             raise ValueError('Parameter attack_ratio should be in range (0, 1)')
 
-        if not 0 < self.attack_probability < 1:
-            raise ValueError('Parameter attack_probability should be in range (0, 1)')
-
         if str(self.attack_label) not in ('-1', '1', '-1.0', '1.0'):
             raise ValueError('Parameter attack_label should be in set {-1, 1}')
         self.attack_label = int(self.attack_label)
 
-        if str(self.training_mode) not in ('0', '1', '0.0', '1.0'):
-            raise ValueError('Parameter training_mode should be in set {0, 1}')
-        self.training_mode = int(self.training_mode)
+        if str(self.processing_mode) not in ('0', '1', '0.0', '1.0'):
+            raise ValueError('Parameter processing_mode should be in set {0, 1}')
+        self.processing_mode = int(self.processing_mode)
 
     def __check_parameters_consistency(self):
-        if self.training_mode == 0:
-            if type(self.training_data) is not str:
-                raise ValueError('Parameter training_data should have type str when training_mode == 0')
-        elif self.training_mode == 1:
-            if type(self.training_data) is str:
-                raise ValueError('Parameter training_data should not have type str when training_mode == 1')
+        if self.processing_mode == 0:
+            if type(self.data) is not str:
+                raise ValueError('Parameter training_data should have type str when processing_mode == 0')
+        elif self.processing_mode == 1:
+            if type(self.data) is str:
+                raise ValueError('Parameter training_data should not have type str when processing_mode == 1')
 
     def __check_parameters_correctness(self):
         self.__check_parameters_type_correctness()
@@ -190,7 +172,6 @@ class DataProcessor:
 
 
 class Model:
-    # todo возможно этот класс лишний
     train_features = None
     test_features = None
     train_labels = None
@@ -226,6 +207,19 @@ class Model:
         except TypeError:
             pass
 
+    def predict_by_proba(self, p_value=0.5, data=None):
+        print('Model named "%s" predicting' % self.name)
+        if data is None:
+            try:
+                pred = self.model.predict_proba(self.test_features)
+                return np.apply_along_axis(
+                    lambda x: 1 if x[0] > p_value else 0, 1, pred
+                ), self.test_labels
+            except AttributeError:
+                raise TypeError('Model "%s" has no predict_proba method.' % self.name)
+        else:
+            return self.model.predict_proba(data), None
+
     def fit(self):
         print('Fitting model named "%s"' % self.name)
         self.model.fit(self.train_features, self.train_labels)
@@ -240,15 +234,80 @@ class Model:
 
 
 class ModelsManager:
-    # todo возможно стоит из него сделать дексриптор или подрубить @property
-    # todo возможно нужно также сделать словарь из метрик качества, только они будут функциями
     models = {}
     data = {}
+    metrics = {}
 
     def __init__(self):
         self.data['kyoto2007'] = DataProcessor('kyoto2007')
         self.load_gradient_boosting()
         self.load_logistic_regression()
+
+        self.add_default_metrics()
+
+    def add_default_metrics(self):
+        default_metric_names = [
+            'average_precision_score',
+            'accuracy_score',
+            'roc_curve',
+            'auc',
+            'precision_recall_fscore_support',
+            'precision_recall_curve'
+        ]
+        default_metrics = [
+            average_precision_score,
+            accuracy_score,
+            roc_curve,
+            auc,
+            precision_recall_fscore_support,
+            precision_recall_curve
+        ]
+        for name, metric in zip(default_metric_names, default_metrics):
+            self.add_metric(name, metric)
+
+    @check_name_correctness
+    def get_metric_result(self, name, model_name, *args, **kwargs):
+
+        def metric_not_found():
+            print('Metric named "%s" doesnt exist' % name)
+            print('Available models:')
+            for metric in self.metrics.keys():
+                print(metric)
+
+        def model_not_found():
+            print('Model named "%s" doesnt exist' % name)
+            print('Available models:')
+            for metric in self.models.keys():
+                print(metric)
+
+        # todo учесть случай, когда data_name подан и отличается от ассоциированного с моделью
+        # todo учесть случай, когда для некоторых метрик нужно изменить знаки лейблов
+
+        if model_name not in self.models.keys():
+            model_not_found()
+            return
+        else:
+            # пока считаем, что все метрики требуют такие обязательные параметры
+            predicted, known = self.models[model_name].predict()
+
+        if name in self.metrics.keys():
+            return self.metrics[name](known, predicted, *args, **kwargs)
+        else:
+            metric_not_found()
+            return
+
+    @check_name_correctness
+    def add_metric(self, name, metric, replace=False):
+
+        if name in self.metrics.keys():
+            if replace:
+                print('Metric named "%s" already exist, proceed with replacing' % name)
+                self.metrics[name] = metric
+            else:
+                print('Metric named "%s" already exist, proceed without replacing' % name)
+        else:
+            print('Add metric named "%s"' % name)
+            self.metrics[name] = metric
 
     @check_name_correctness
     def add_model(self, name, model, data_name, replace=False):
@@ -292,31 +351,6 @@ class ModelsManager:
             print('Add data named "%s" ' % name)
             really_add_data(self.data)
 
-    @check_name_correctness
-    def get_model_average_precision_score(self, name, data_name=None):
-        # todo учесть случай, когда data_name подан и отличается от ассоциированного с моделью
-        predicted, known = self.models[name].predict()
-        return average_precision_score(predicted, known)
-
-    @check_name_correctness
-    def get_model_precision_recall_fscore_support(self, name):
-        pass
-
-    @check_name_correctness
-    def get_model_cross_validation(self, name):
-        pass
-
-    @check_name_correctness
-    def get_model_roc_curve(self, name):
-        pass
-
-    @check_name_correctness
-    def get_model_auc(self, name):
-        pass
-
-    def load_local_data(self):
-        pass
-
     def load_gradient_boosting(self):
         self.add_model('gb', GradientBoostingClassifier, 'kyoto2007')
 
@@ -325,4 +359,4 @@ class ModelsManager:
 
 
 mm = ModelsManager()
-print(mm.get_model_average_precision_score('lr'))
+print(mm.get_metric_result('average_precision_score', 'gb'))
